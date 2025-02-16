b86ccb91535b32227a0bac8d71d9c6343b158ed2
mehdizebhi <mehdizebhi@yahoo.com>
2025-02-16 14:09:32 +0330

    Add new spring boot project

---
+++ src/main/java/dev/mehdizebhi/web3/Application.java
@@ -0,0 +1,13 @@
+package dev.mehdizebhi.web3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootWeb3DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootWeb3DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mehdizebhi/web3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mehdizebhi.web3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootWeb3DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
